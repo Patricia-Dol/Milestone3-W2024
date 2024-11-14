10a7044e01a10ee81981d5786e01e55beaffecc8
Patricia-Dol <pattid1608@gmail.com>
2024-11-13 20:28:06 -0500

    Added password hashing to the app

---
+++ HPlusSport.Security.Web/Migrations/20241114012544_Init2.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HPlusSport.Security.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Hash", "Password", "Salt" },
+                values: new object[] { "WMut3QlwLJMXQC5ruk9dWpLhKts=", "", "qTTkQgS6ksz3D7P6qNjbGWJmDxMp53CzsM4FpZDExXg=" });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Hash", "Password", "Salt" },
+                values: new object[] { "aPnpdCdqU/ugdsmajYsRsCYjyyo=", "", "8Oe0cd5GG5IznafXJrvvvhKmedFSW5w2YfOdNf3WKuc=" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Hash", "Password", "Salt" },
+                values: new object[] { "", "Adam's secret", "" });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Hash", "Password", "Salt" },
+                values: new object[] { "", "b@rb@r@", "" });
+        }
+    }
+}
