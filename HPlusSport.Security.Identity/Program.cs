70dd74bbd19b6378669df6ca80ed5a874aa457b9
Patricia <pattid1608@gmail.com>
2024-11-22 23:09:17 -0500

    Modify the project file to match the project file in Securing APIs and added the nugget package

--- HPlusSport.Security.Identity/Program.cs
+++ HPlusSport.Security.Identity/Program.cs
@@ -1,6 +1,7 @@
 using HPlusSport.Security.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace HPlusSport.Security.Identity
 {
@@ -11,6 +12,9 @@ namespace HPlusSport.Security.Identity
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
@@ -18,6 +22,31 @@ namespace HPlusSport.Security.Identity
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.ConfigureApplicationCookie(o =>
+            {
+                o.Events = new CookieAuthenticationEvents()
+                {
+                    OnRedirectToLogin = (ctx) =>
+                    {
+                        if (ctx.Request.Path.StartsWithSegments("/api") && ctx.Response.StatusCode == 200)
+                        {
+                            ctx.Response.StatusCode = 401;
+                        }
+
+                        return Task.CompletedTask;
+                    },
+                    OnRedirectToAccessDenied = (ctx) =>
+                    {
+                        if (ctx.Request.Path.StartsWithSegments("/api") && ctx.Response.StatusCode == 200)
+                        {
+                            ctx.Response.StatusCode = 403;
+                        }
+
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
             builder.Services.AddRazorPages();
 
             var app = builder.Build();
@@ -26,6 +55,8 @@ namespace HPlusSport.Security.Identity
             if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
             else
             {
@@ -42,6 +73,8 @@ namespace HPlusSport.Security.Identity
             app.UseAuthorization();
 
             app.MapRazorPages();
+
+            app.MapSwagger();
 
             app.Run();
         }
